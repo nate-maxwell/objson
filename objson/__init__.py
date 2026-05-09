0376683f120974e08f5c8589ad2fdb44bc63e957
nate-maxwell <natedmaxwell@gmail.com>
2026-05-09 13:37:46 -0700

    latest

--- objson/__init__.py
+++ objson/__init__.py
@@ -1,2 +1,6 @@
 from objson.serialize import dumps
 from objson.serialize import loads
+from objson.serialize import load
+from objson.serialize import loads
+from objson.serialize import dump
+from objson.serialize import dumps
